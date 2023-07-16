6303dfa6e3d27bb9c8e8b6e1f9c161761d0c94a8
Lars Wunderlich <lars.wunderlich2003@web.de>
2023-07-16 15:01:23 +0200

    #2 implemented Entry class

---
+++ classes/Entry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Entry {
+    public Entry(string Type, string Name, DateOnly BestBeforeDate, DateOnly? BuyDate, uint Amount, double? Prize) {
+        this.Type = Type;
+        this.Name = Name;
+        this.BestBeforeDate = BestBeforeDate;
+        this.BuyDate = BuyDate;
+        this.Amount = Amount;
+        this.Prize = Prize;
+    }
+    public string Type {get;}
+    public string Name {get;}
+    public DateOnly BestBeforeDate {get;}
+    public DateOnly? BuyDate {get;}
+    public uint Amount {get;}
+    public double? Prize {get;}
+}
