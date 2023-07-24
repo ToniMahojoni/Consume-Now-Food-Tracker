80bd10d094525c94e59c73f73c05457ab9c3c55e
Toni Mahoni <sand.tonidaniel@gmail.com>
2023-07-24 17:34:57 +0200

    #34 added comments to LebensmittelAddPage

--- ConsumeNow/Subpages/LebensmittelAddPage.xaml.cs
+++ ConsumeNow/Subpages/LebensmittelAddPage.xaml.cs
@@ -15,14 +15,14 @@ namespace ConsumeNow.Subpages
             InitializeComponent();
         }
 
+        //boolean that represents the mode
+        //false == add mode
+        //true == edit mode
         private bool mode = false;
 
         private void WindowLoadedLebensmittelAddPage(object sender, RoutedEventArgs e)
         {
-            foreach (var element in MainWindow.types)
-            {
-                TypCB.Items.Add(element.Name);
-            }
+            fillTypComboBox();
         }
 
         private void OpenableChecked(object sender, RoutedEventArgs e)
@@ -32,6 +32,7 @@ namespace ConsumeNow.Subpages
                 {
                     if (openableCB.IsChecked == true)
                     {
+                        //show the further options for openable food
                         geöffnetTL.Visibility = Visibility.Visible;
                         verbleibendTL.Visibility = Visibility.Visible;
                         openedCB.Visibility = Visibility.Visible;
@@ -39,6 +40,7 @@ namespace ConsumeNow.Subpages
                     }
                     else
                     {
+                        //hide the further options for unopenable food
                         geöffnetTL.Visibility = Visibility.Collapsed;
                         verbleibendTL.Visibility = Visibility.Collapsed;
                         openedCB.Visibility = Visibility.Collapsed;
@@ -49,6 +51,7 @@ namespace ConsumeNow.Subpages
 
         private void EmptyData()
         {
+            //clear all input-fields
             TypCB.SelectedItem = null;
             NameCB.SelectedItem = null;
             HaltbarkeitTB.Text = string.Empty;
@@ -66,19 +69,25 @@ namespace ConsumeNow.Subpages
             string[] EntryData;
             if (openableCB.IsChecked == true)
             {
+                //if food is openable, it gets saved as AdvancedEntry
+                //array length is 8 in this case
                 EntryData = new string[8];
             }
             else
             {
+                //if food is unopenable, it gets saved as Entry
+                //array length is 6 in this case
                 EntryData = new string[6];
             }
 
             if (TypCB.SelectedItem != null)
             {
+                //inserted data from the input-fields is getting written in the EntryData array
                 EntryData[0] = TypCB.SelectedItem.ToString();
 
                 if (NameCB.SelectedItem == null)
                 {
+                    //when there is no name selected, the name is set to the type
                     EntryData[1] = TypCB.SelectedItem.ToString();
                 }
                 else
@@ -90,6 +99,7 @@ namespace ConsumeNow.Subpages
 
                 if (KaufdatumTB.Text == string.Empty)
                 {
+                    //if there is no buy date selected, the current date is set as buy date
                     EntryData[3] = DateOnly.FromDateTime(DateTime.Today).ToString();
                 }
                 else
@@ -97,15 +107,13 @@ namespace ConsumeNow.Subpages
                     EntryData[3] = KaufdatumTB.Text.ToString();
                 }
 
-
                 EntryData[4] = MengeTB.Text.ToString();
                 EntryData[5] = PreisTB.Text.ToString();
             }
 
-
-
             if (EntryData.Length == 8)
             {
+                //additional data for the AdvancedEntries
                 EntryData[6] = openedCB.IsChecked.ToString();
                 EntryData[7] = verbleibendTB.Text.ToString();
             }
@@ -115,20 +123,24 @@ namespace ConsumeNow.Subpages
             {
                 if (mode)
                 {
+                    //editing mode
+                    //saving changes the general entries list
                     ManageDatabase.EditEntry(EntryData, MainWindow.entries,
                         Convert.ToUInt32(IDTB.Text.ToString()));
+                    //clear ID TextBox
                     IDTB.Text = String.Empty;
                 }
                 else
                 {
+                    //saving as a new entry
                     ManageDatabase.AddEntry(EntryData, MainWindow.entries);
-                    
                 }
                 DatabaseIO.SaveToDatabase<Entry>(MainWindow.entries, MainWindow.entryfilepath);
                 SuccessfullSave(sender, e);
             }
             catch
             {
+                //show failed message
                 SaveInfoTL.Visibility = Visibility.Visible;
                 SaveInfo.Content = "fehlgeschlagen!";
                 SaveInfo.Foreground = Brushes.Red;
@@ -139,6 +151,7 @@ namespace ConsumeNow.Subpages
         {
             if (TypCB.SelectedItem != null)
             {
+                //show all input-fields
                 NameCB.Visibility = Visibility.Visible;
                 HaltbarkeitTB.Visibility = Visibility.Visible;
                 KaufdatumTB.Visibility = Visibility.Visible;
@@ -146,6 +159,7 @@ namespace ConsumeNow.Subpages
                 PreisTB.Visibility = Visibility.Visible;
                 openableCB.Visibility = Visibility.Visible;
 
+                //show all TextLabels
                 NameTL.Visibility = Visibility.Visible;
                 HaltbarkeitTL.Visibility = Visibility.Visible;
                 KaufdatumTL.Visibility = Visibility.Visible;
@@ -153,29 +167,38 @@ namespace ConsumeNow.Subpages
                 PreisTL.Visibility = Visibility.Visible;
                 openableTL.Visibility = Visibility.Visible;
 
+                //show save button and hide edit button and ID input-field
                 SpeichernButton.Visibility = Visibility.Visible;
                 EditierenButton.Visibility = Visibility.Collapsed;
                 IDTB.Visibility = Visibility.Collapsed;
                 IDTL.Visibility = Visibility.Collapsed;
                 EditierenTL.Visibility = Visibility.Collapsed;
 
+                //disable Typ CheckBox after selecting the type
                 TypCB.IsEnabled = false;
+
+                //hide confirm button and save info TextLabel
                 BestätigenButton.Visibility = Visibility.Collapsed;
                 SaveInfoTL.Visibility = Visibility.Collapsed;
 
+                //mode is set to editing mode
                 mode = false;
 
-                fillComboBox();
+                fillNameComboBox();
             }
         }
 
         private void EditierenButtonClick(object sender, RoutedEventArgs e)
         {
+            //hide save info TextLabel
             SaveInfoTL.Visibility = Visibility.Collapsed;
             
             try
             {
+            //load data from the entry with the selected ID
             LoadEditData(Convert.ToUInt32(IDTB.Text.ToString()), sender, e);
+
+            //show and enable all input-fields
             TypCB.Visibility = Visibility.Visible;
             TypCB.IsEnabled = true;
             NameCB.Visibility = Visibility.Visible;
@@ -185,6 +208,7 @@ namespace ConsumeNow.Subpages
             PreisTB.Visibility = Visibility.Visible;
             openableCB.Visibility = Visibility.Visible;
 
+            //show all TextLabels
             NameTL.Visibility = Visibility.Visible;
             HaltbarkeitTL.Visibility = Visibility.Visible;
             KaufdatumTL.Visibility = Visibility.Visible;
@@ -192,6 +216,7 @@ namespace ConsumeNow.Subpages
             PreisTL.Visibility = Visibility.Visible;
             openableTL.Visibility = Visibility.Visible;
 
+            //show save button and hide other buttons and ID input-field
             SpeichernButton.Visibility = Visibility.Visible;
             IDTB.Visibility = Visibility.Collapsed;
             IDTL.Visibility = Visibility.Collapsed;
@@ -199,18 +224,21 @@ namespace ConsumeNow.Subpages
             BestätigenButton.Visibility = Visibility.Collapsed;
             EditierenButton.Visibility = Visibility.Collapsed;
 
+            //hide add TextLabel in the top left corner
             hinzufügenTL.Visibility = Visibility.Collapsed;
-            fillComboBox();
+
+            fillNameComboBox();
             }
             catch
             {
+                //show failed message
                 SaveInfo.Content = "fehlgeschlagen!";
                 SaveInfo.Visibility = Visibility.Visible;
                 SaveInfoTL.Visibility = Visibility.Visible;
                 SaveInfo.Foreground = Brushes.Red;
             }
             
-
+            //mode is set to edit mode
             mode = true;
         }
 
@@ -218,26 +246,33 @@ namespace ConsumeNow.Subpages
         {
             EmptyData();
 
+            //show saved message
             SaveInfoTL.Visibility = Visibility.Visible;
             SaveInfo.Content = "gespeichert!";
             SaveInfo.Foreground = Brushes.Green;
 
+            //create standard configuration for openable CheckBox
             OpenableChecked(sender, e);
 
+            //clear all input-fields
             LebensmittelAddReset();
 
+            //reload LebensmittelPage to make the changes visible
             MainWindow.lebensmittelpage.WindowLoadedLebensmittelPage(sender, e);
 
+            //hide save button
             SpeichernButton.Visibility = Visibility.Collapsed;
         }
 
-        private void fillComboBox()
+        private void fillNameComboBox()
         {
+            //pick type with selected name
             var SelectedSubnames =
                     from type in MainWindow.types
                     where type.Name == TypCB.SelectedItem.ToString()
                     select type.Subnames;
 
+            //fill all type names into the subnames ComboBox
             foreach (var subnames in SelectedSubnames)
             {
                 foreach (string subname in subnames)
@@ -247,10 +282,22 @@ namespace ConsumeNow.Subpages
             }
         }
 
+        private void fillTypComboBox()
+        {
+            //fill the type ComboBox with all types from the general type list
+            foreach (var element in MainWindow.types)
+            {
+                TypCB.Items.Add(element.Name);
+            }
+        }
+
         public void LebensmittelAddReset()
         {
+            //clear the data on the input-fields
             EmptyData();
 
+            //show the add TextLabel at the top left corner
+            //and hide all other input-fields
             hinzufügenTL.Visibility = Visibility.Visible;
             NameCB.Visibility = Visibility.Collapsed;
             HaltbarkeitTB.Visibility = Visibility.Collapsed;
@@ -263,8 +310,7 @@ namespace ConsumeNow.Subpages
             verbleibendTB.Visibility = Visibility.Collapsed;
             verbleibendTL.Visibility = Visibility.Collapsed;
 
-
-
+            //hide all TextLabels
             NameTL.Visibility = Visibility.Collapsed;
             HaltbarkeitTL.Visibility = Visibility.Collapsed;
             KaufdatumTL.Visibility = Visibility.Collapsed;
@@ -272,13 +318,14 @@ namespace ConsumeNow.Subpages
             PreisTL.Visibility = Visibility.Collapsed;
             openableTL.Visibility = Visibility.Collapsed;
             
-
+            //hide save button and show all other buttons and ID input-field
             SpeichernButton.Visibility = Visibility.Collapsed;
             EditierenButton.Visibility = Visibility.Visible;
             IDTB.Visibility = Visibility.Visible;
             IDTL.Visibility = Visibility.Visible;
             EditierenTL.Visibility = Visibility.Visible;
 
+            //enable type CheckBox and show confirm button
             TypCB.IsEnabled = true;
             BestätigenButton.Visibility = Visibility.Visible;
             
@@ -286,8 +333,10 @@ namespace ConsumeNow.Subpages
 
         private void LoadEditData(uint ID, object sender, RoutedEventArgs e)
         {
+            //select entry with the inserted ID
             var entry = ManageDatabase.SelectEntry(MainWindow.entries, ID);
 
+            //fill the TextBoxes with the entry data
             NameCB.SelectedItem = entry.Name;
             TypCB.SelectedItem = entry.Type;
             HaltbarkeitTB.Text = entry.BestBeforeDate.ToString();
@@ -297,9 +346,11 @@ namespace ConsumeNow.Subpages
             openableCB.IsChecked = entry is AdvancedEntry;
             if (entry is AdvancedEntry)
             {
+                //data of the AdvancedEntry
                 openedCB.IsChecked = (entry as AdvancedEntry).IsOpened;
                 verbleibendTB.Text = Convert.ToString((entry as AdvancedEntry).RemainingAmount, System.Globalization.CultureInfo.InvariantCulture);
             }
+            //check if entry is openable and then display the neccessary TextBoxes and input-fields
             OpenableChecked(sender, e);
         }
     }
