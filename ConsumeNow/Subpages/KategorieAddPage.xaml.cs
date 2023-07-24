44e7c13e81a3d98b07969b4039166dc1440d46cf
Toni Mahoni <sand.tonidaniel@gmail.com>
2023-07-24 17:01:24 +0200

    #34 added comments to KategorieAddPage

--- ConsumeNow/Subpages/KategorieAddPage.xaml.cs
+++ ConsumeNow/Subpages/KategorieAddPage.xaml.cs
@@ -13,15 +13,23 @@ namespace ConsumeNow.Subpages
 
         private void SaveButtonClick(object sender, RoutedEventArgs e) { 
             string[] TypeData = new string[6];
+
+            //fill the TypeData array with the data from the input-fields
             TypeData[0] = NameTB.Text.ToString();
             TypeData[1] = LagerortTB.Text.ToString();
             TypeData[2] = EinkaufslisteHinzufügenTB.Text.ToString();
             TypeData[3] = ÄnderungMhDTB.Text.ToString();
             TypeData[4] = ProdukteTB.Text.ToString();
             TypeData[5] = "0";
+
             try {
+
+                //add the data to the general types array
                 Database.ManageDatabase.AddType(TypeData,MainWindow.types);
+                //save the changes to the file
                 Database.DatabaseIO.SaveToDatabase<Database.Type>(MainWindow.types, MainWindow.typefilepath);
+
+                //clear input-fields and show successful message
                 NameTB.Text = string.Empty;
                 LagerortTB.Text = string.Empty;
                 EinkaufslisteHinzufügenTB.Text = string.Empty;
@@ -31,6 +39,8 @@ namespace ConsumeNow.Subpages
                 SaveInfoTL.Content = "erfolgreich!";
                 SaveInfoTL.Foreground = Brushes.Green;
             } catch {
+
+                //show failed message
                 SaveInfoBR.Visibility = Visibility.Visible;
                 SaveInfoTL.Content = "fehlgeschlagen!";
                 SaveInfoTL.Foreground = Brushes.Red;
