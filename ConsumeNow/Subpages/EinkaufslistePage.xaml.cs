53aabf470dab95c09bf5e900aaff4c5f2a6572d9
Toni Mahoni <sand.tonidaniel@gmail.com>
2023-07-24 16:58:37 +0200

    #34 added comments to EinkaufslistePage

--- ConsumeNow/Subpages/EinkaufslistePage.xaml.cs
+++ ConsumeNow/Subpages/EinkaufslistePage.xaml.cs
@@ -27,14 +27,18 @@ namespace ConsumeNow
 
         public void WindowLoadedEinkaufslistePage(object sender, RoutedEventArgs e)
         {
+            //fills the table with the data from the shopping list file
             System.Data.DataTable dt3 = new System.Data.DataTable();
 
             string[] ColumnNames3 = { "Menge", "Typ"};
 
+            //creation of the columns
             foreach (string ColumnName in ColumnNames3)
             {
                 dt3.Columns.Add(ColumnName, typeof(string));
             }
+
+            //creation of the rows
             foreach(var rowData in ShoppingList.GetShoppingList(MainWindow.types))
             {
                 dt3.Rows.Add(rowData.Split(' '));
